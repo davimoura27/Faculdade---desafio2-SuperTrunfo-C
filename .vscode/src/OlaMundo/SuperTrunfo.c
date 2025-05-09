e005d0e71fc29bbcc2f6bf8be6e984b4eb642f70
davimoura27 <davimoura27@gmail.com>
2025-05-08 22:29:42 -0300

    Desafio tema 2

--- .vscode/src/OlaMundo/SuperTrunfo.c
+++ .vscode/src/OlaMundo/SuperTrunfo.c
@@ -9,7 +9,7 @@ unsigned long int populacao1;
 float area1;
 float pib1;
 int pontosTuristicos1;
-float densidade1,pibPerCapita1, superPoder1;
+float densidade1,pibPerCapita1;
  
 
 printf("Carta 1:\n");
@@ -30,8 +30,7 @@ scanf("%d", &pontosTuristicos1);
 
 densidade1 = (float)populacao1 / area1;
 pibPerCapita1 = (pib1 * 1000000000.0f) / populacao1;
-superPoder1 = (float)populacao1 + area1 + (pib1 * 1000000000.0f) + pontosTuristicos1 
-+ pibPerCapita1 + (1.0f / densidade1);
+
 
 // Carta 2
 char estado2;
@@ -41,7 +40,7 @@ unsigned long int populacao2;
 float area2;
 float pib2;
 int pontosTuristicos2;
-float densidade2, pibPerCapita2, superPoder2;
+float densidade2, pibPerCapita2;
 
 printf("\nCarta 2:\n");
 printf("Estado: ");
@@ -61,19 +60,20 @@ scanf("%d", &pontosTuristicos2);
 
 densidade2 = (float)populacao2 / area2;
 pibPerCapita2 = (pib2 * 1000000000.0f) / populacao2;
-superPoder2 = (float)populacao2 + area2 + (pib2 * 1000000000.0f) + pontosTuristicos2 + pibPerCapita2 + (1.0f / densidade2);
 
 //Exibição
 
 printf("\nComparação de Cartas:\n");
 
-printf("População: Carta 1 venceu (%d)\n", populacao1 > populacao2);
-printf("Área: Carta 1 venceu (%d)\n", area1 > area2);
-printf("PIB: Carta 1 venceu (%d)\n", pib1 > pib2);
-printf("Pontos Turísticos: Carta 1 venceu (%d)\n", pontosTuristicos1 > pontosTuristicos2);
-printf("Densidade Populacional: Carta 1 venceu (%d)\n", densidade1 < densidade2);
-printf("PIB per Capita: Carta 1 venceu (%d)\n", pibPerCapita1 > pibPerCapita2);
-printf("Super Poder: Carta 1 venceu (%d)\n", superPoder1 > superPoder2);
+if (populacao1 > populacao2){
+    printf("Carta 1 - São Paulo (SP):%f \n", densidade1);
+    printf("Carta 2 - Rio de Janeiro:%f \n", densidade2);
+    printf("Carta 1 (São Paulo) venceu!\n");
+}else{
+   printf("Carta 1 - São Paulo (SP):%f\n", densidade1);
+    printf("Carta 2 - Rio de Janeiro:%f\n", densidade2);
+    printf("Carta 2 (Rio de Janeiro) venceu!\n");
+}
 
 
 return 0;
